09d1bbb4e0b9a6b993eb18473a7fc68f99f0754e
ablimit aji <aaji@ubuntu.(none)>
2011-12-25 13:14:57 -0500

    a code template for quick start added.

---
+++ interviewstreet/codetemplate.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+import sys
+
+def main():
+
+    if len(sys.argv) <2:
+	print "Missing argument"
+	sys.exit(0)
+
+    path   = sys.argv[1]
+
+    TAB="\t";
+    SPACE=" ";
+
+if __name__ == '__main__':
+    main()
+
